08d6099e17ef273b466e2de122058d93e9d83dec
Paweł Wypych <92308173+pawel00wypych@users.noreply.github.com>
2026-04-12 20:17:03 +0200

    Add: sql-sellers-with-no-sales - submission-0

---
+++ SQL/sql-sellers-with-no-sales/submission-0.sql
@@ -0,0 +1,9 @@
+-- Write your query below
+SELECT s.seller_name
+FROM seller s
+LEFT JOIN orders o 
+    ON o.seller_id = s.seller_id
+    AND o.sale_date >= '2020-01-01'
+    AND o.sale_date < '2021-01-01'
+WHERE o.customer_id IS NULL
+ORDER BY s.seller_name;
