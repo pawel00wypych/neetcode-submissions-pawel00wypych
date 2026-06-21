3b610d812e6fcf0e8b7f752d1edb771a58b063ca
Paweł Wypych <92308173+pawel00wypych@users.noreply.github.com>
2026-06-21 10:38:06 +0200

    Add: maximum-distance-in-arrays - submission-8

---
+++ Data Structures & Algorithms/maximum-distance-in-arrays/submission-8.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxDistance(self, arrays: List[List[int]]) -> int:
+        
+        visited_indexes = set()
+
+        max_diff = 0
+        diff1 = abs(arrays[0][0] - arrays[1][-1])
+        diff2 = abs(arrays[1][0] - arrays[0][-1])
+        if diff2 > diff1:
+            max_diff = diff2
+            global_min = arrays[1][0]
+            global_max = arrays[0][-1]
+        else:
+            max_diff = diff1
+            global_min = arrays[0][0]
+            global_max = arrays[1][-1]
+
+        for m in range(2, len(arrays)):
+            if m not in visited_indexes and arrays[m][0] < global_min and max_diff < abs(global_max-arrays[m][0]):
+                global_min = arrays[m][0]
+                visited_indexes.add(m)
+                max_diff = abs(global_max - global_min)
+            elif m not in visited_indexes and arrays[m][-1] > global_max and max_diff < abs(arrays[m][-1] - global_min):
+                global_max = arrays[m][-1]
+                max_diff = abs(global_max - global_min)
+                visited_indexes.add(m)
+
+        return max_diff
